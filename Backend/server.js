64484a7462897bcd30d4f6e009eb05904f893422
amjadali448 <crazybird146@gmail.com>
2023-07-26 14:40:16 +0500

    some api's added

--- Backend/server.js
+++ Backend/server.js
@@ -51,6 +51,20 @@ app.post ('/inspect-container',async(req,res)=>{
     }
 });
 
+app.post ('/container-logs',async(req,res)=>{
+  try{
+    const dockerApiUrl = `http://127.0.0.1:2375/containers/nervous_spence/logs?stderr=true`;
+        const dockerApiresponse = await axios.get(dockerApiUrl,{httpsAgent});
+        console.log(dockerApiresponse.data);
+        res.json(dockerApiresponse.data);
+    } 
+    catch (error) 
+    {
+        console.error('connection error:', error);
+        res.status(500).send('Error connecting to the Docker');
+    }
+});
+
 app.post ('/container-list',async(req,res)=>{
   try{
     const dockerApiUrl = `http://127.0.0.1:2375/containers/json?all=true`;
@@ -102,6 +116,91 @@ app.post('/Create-container', async (req, res) => {
   }
 });
 
+// issue in starting
+app.post('/start-container', async (req, res) => {
+  try {
+    const startContainerApi = `http://127.0.0.1:2375/containers/objective_nash/start`;
+
+    const dockerApiResponse = await axios.post(startContainerApi,{ httpsAgent });
+    console.log(dockerApiResponse.data);
+    res.json(dockerApiResponse.data);
+  } catch (error) {
+    console.error('Container Error:', error);
+    res.status(500).send('Error Starting Docker Container');
+  }
+});
+
+app.post('/stop-container', async (req, res) => {
+  try {
+    const stopContainerApi = 'http://127.0.0.1:2375/containers/lucid_cartwright/stop';
+    const dockerApiResponse = await axios.post(stopContainerApi,{ httpsAgent });
+    console.log(dockerApiResponse.data);
+    res.json(dockerApiResponse.data);
+  } catch (error) {
+    if (error.response && error.response.status === 304) {
+      res.status(200).send('Container was already stopped.');
+    }
+    else{
+      console.error('Container Error:', error);
+      res.status(500).send('Error Stopping Docker Container');
+    }
+  }
+});
+
+// issue in restart
+app.post('/restart-container', async (req, res) => {
+  try {
+    const restartContainerApi = 'http://127.0.0.1:2375/containers/lucid_cartwright/restart';
+    const dockerApiResponse = await axios.post(restartContainerApi,{ httpsAgent });
+    console.log(dockerApiResponse.data);
+    res.json(dockerApiResponse.data);
+  } catch (error) {
+      console.error('Container Error:', error);
+      res.status(500).send('Error Restarting Docker Container');
+  }
+});
+
+// issue in removing
+app.delete('/removing-container', async (req, res) => {
+  try {
+    const removeContainerApi = 'http://127.0.0.1:2375/containers/1a06535b763f';
+    const dockerApiResponse = await axios.post(removeContainerApi,{ httpsAgent });
+    console.log(dockerApiResponse.data);
+    res.json(dockerApiResponse.data);
+  } catch (error) {
+      console.error('Container Error:', error);
+      res.status(500).send('Error Removing Docker Container');
+  }
+});
+
+app.post('/delete-stopped-containers', async (req, res) => {
+  try {
+    const deleteContainerApi = 'http://127.0.0.1:2375/containers/prune';
+    const dockerApiResponse = await axios.post(deleteContainerApi,{ httpsAgent });
+    console.log(dockerApiResponse.data);
+    res.json(dockerApiResponse.data);
+  } catch (error) {
+      console.error('Container Error:', error);
+      res.status(500).send('Error deleting Docker Containers');
+  }
+});
+
+app.post('/pause-containers', async (req, res) => {
+  try {
+    const pauseContainerApi = 'http://127.0.0.1:2375/containers/flamboyant_goldberg/pause';
+    const dockerApiResponse = await axios.post(pauseContainerApi,{ httpsAgent });
+    console.log(dockerApiResponse.data);
+    res.json(dockerApiResponse.data);
+  } catch (error) {
+    if (error.response && error.response.status === 409) {
+      res.status(200).send('Container is not running.');
+    }
+    else{
+      console.error('Container Error:', error);
+      res.status(500).send('Error Stopping Docker Container');
+    }
+  }
+});
 
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
